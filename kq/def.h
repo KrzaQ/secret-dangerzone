0c79820adc01d30c7b868e0312e4eba8638054c0
KrzaQ <git@krzaq.cc>
2013-02-03 20:46:10 +0100

    Adding definitions file.

---
+++ kq/def.h
@@ -0,0 +1,32 @@
+#ifndef KQDEF_H
+#define KQDEF_H
+
+#include <cstdint>
+
+typedef std::uint64_t			QWORD;
+typedef std::uint32_t			DWORD;
+typedef std::uint16_t			WORD;
+typedef std::uint8_t			BYTE;
+
+typedef long double				ldouble;
+
+typedef unsigned long long		ullong;
+typedef unsigned long			ulong;
+typedef unsigned int			uint;
+typedef unsigned short			ushort;
+typedef unsigned char			uchar;
+
+// char is a different type than signed char
+typedef unsigned char			schar;
+
+typedef std::uint64_t			u64;
+typedef std::uint32_t			u32;
+typedef std::uint16_t			u16;
+typedef std::uint8_t			u8;
+
+typedef std::int64_t			i64;
+typedef std::int32_t			i32;
+typedef std::int16_t			i16;
+typedef std::int8_t				i8;
+
+#endif // KQDEF_H
